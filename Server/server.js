3a2169e47043e2eecc15a7aec61b88e81c7040f1
David Urting <David.Urting@lv.vlaanderen.be>
2024-05-12 00:10:16 +0200

    cors aanpassing

--- Server/server.js
+++ Server/server.js
@@ -16,14 +16,13 @@ const port = process.env.PORT ? process.env.PORT : 3010;
 // De static files middleware registreren
 app.use("/", express.static(join(__dirname, 'public','browser')));
 
-// CORS is vereist omdat er tijdens ontwikkeling calls komen van een andere host:port (poort 4200) naar deze host:port (poort 3010)
-app.use(cors());
 app.use(express.json());
 
 // REST/WEB API - start
+// CORS is vereist omdat er tijdens ontwikkeling calls komen van een andere host:port (poort 4200) naar deze host:port (poort 3010)
 const myClocks = [new Klok('thuis', 'Europe/Brussels', 'nl-BE')];
 
-app.get('/my-clocks', function (req, res) {
+app.get('/my-clocks', cors(), function (req, res) {
   res.json(myClocks);
 })
 // REST/WEB API - end
